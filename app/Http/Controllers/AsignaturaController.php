4d2fde8ae482e6ee7eaa99441cdb7daf0c4b65ab
Jigsaw123ixas2 <vichomr2001@gmail.com>
2020-11-22 15:08:41 -0400

    Create AsignaturaController.php

---
+++ app/Http/Controllers/AsignaturaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AsignaturaController extends Controller
+{
+    public function asignatura(){
+
+        return "Hola mundo";
+    }
+    public function inscripcion(){
+        return "chao mundo";
+    }
+}
